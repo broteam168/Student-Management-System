f142c7cd1adf05ef038c3f83bfcaf6921a86cb09
pc <pc@LAPTOP-MBMH7EOM>
2023-12-20 02:13:33 +0700

    Add project files.

---
+++ StudentManager.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManager.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
